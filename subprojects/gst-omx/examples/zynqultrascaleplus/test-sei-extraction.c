896935df295cc0047e4d8edf323c36a1fd526897
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-02-25 15:01:04 +0100

    test-sei-extraction: display frame number

--- subprojects/gst-omx/examples/zynqultrascaleplus/test-sei-extraction.c
+++ subprojects/gst-omx/examples/zynqultrascaleplus/test-sei-extraction.c
@@ -1,5 +1,7 @@
 #include <gst/gst.h>
 
+static guint frame_count = 0;
+
 static gboolean
 bus_call (GstBus * bus, GstMessage * msg, gpointer data)
 {
@@ -37,8 +39,8 @@ bus_call (GstBus * bus, GstMessage * msg, gpointer data)
 
       g_assert (gst_structure_get (s, "payload-type", G_TYPE_UINT, &sei_type,
               "payload", GST_TYPE_BUFFER, &buf, NULL));
-      g_print ("Parsed SEI (type=%d size=%" G_GSIZE_FORMAT "):\n", sei_type,
-          gst_buffer_get_size (buf));
+      g_print ("Parsed SEI after frame %u (type=%d size=%" G_GSIZE_FORMAT
+          "):\n", frame_count, sei_type, gst_buffer_get_size (buf));
       gst_util_dump_buffer (buf);
 
       gst_buffer_unref (buf);
@@ -51,12 +53,20 @@ bus_call (GstBus * bus, GstMessage * msg, gpointer data)
   return TRUE;
 }
 
+static GstPadProbeReturn
+sink_buffer_cb (GstPad * pad, GstPadProbeInfo * info, gpointer user_data)
+{
+  frame_count++;
+  return GST_PAD_PROBE_OK;
+}
+
 int
 main (int argc, char *argv[])
 {
   GError *error = NULL;
   GMainLoop *loop;
-  GstElement *pipeline;
+  GstElement *pipeline, *sink;
+  GstPad *pad;
   gchar *desc;
   GstBus *bus;
 
@@ -72,13 +82,21 @@ main (int argc, char *argv[])
   g_print ("Opening %s\n", argv[1]);
   desc =
       g_strdup_printf
-      ("filesrc location=%s ! h265parse ! omxh265dec ! fakevideosink", argv[1]);
+      ("filesrc location=%s ! h265parse ! omxh265dec ! fakevideosink name=sink",
+      argv[1]);
   pipeline = gst_parse_launch (desc, &error);
   g_assert_no_error (error);
   g_free (desc);
 
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
   gst_bus_add_watch (bus, bus_call, loop);
+
+  sink = gst_bin_get_by_name (GST_BIN (pipeline), "sink");
+  pad = gst_element_get_static_pad (sink, "sink");
+  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER, sink_buffer_cb, NULL,
+      NULL);
+  gst_object_unref (pad);
+  gst_object_unref (sink);
 
   g_print ("Running...\n");
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
