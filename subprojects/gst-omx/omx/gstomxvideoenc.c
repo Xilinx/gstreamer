25ac57fb35c1e41253942cc4b2843ae84626282c
Jeegar Patel <jeegar.patel@xilinx.com>
2019-03-01 06:23:03 -0800

    omxvideoenc: Add support of various color primaries formats
    Use GStreamer color primaries
    
    Upstream-Status: Pending
    Signed-off-by: Jeegar Patel <jeegar.patel@xilinx.com>
    Signed-off-by: Stéphane Cerveay <scerveau@collabora.com>

--- subprojects/gst-omx/omx/gstomxvideoenc.c
+++ subprojects/gst-omx/omx/gstomxvideoenc.c
@@ -25,7 +25,7 @@
 #include <gst/gst.h>
 #include <gst/video/gstvideometa.h>
 #include <gst/allocators/gstdmabuf.h>
-
+#include <gst/video/video.h>
 #include <string.h>
 
 #include "gstomxbufferpool.h"
@@ -654,6 +654,68 @@ gst_omx_video_enc_init (GstOMXVideoEnc * self)
         gst_omx_error_to_string (err), err); \
     return FALSE; \
   }
+
+static gboolean
+gst_omx_video_enc_set_color_primaries (GstOMXVideoEnc * self)
+{
+  OMX_ALG_VIDEO_PARAM_COLOR_PRIMARIES param;
+  GstVideoInfo *info = &self->input_state->info;
+  GstVideoColorimetry cinfo = GST_VIDEO_INFO_COLORIMETRY (info);
+  OMX_ERRORTYPE err;
+
+  GST_OMX_INIT_STRUCT (&param);
+  param.nPortIndex = self->enc_in_port->index;
+
+  switch (cinfo.primaries) {
+    case GST_VIDEO_COLOR_PRIMARIES_BT709:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_BT_709;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_BT470M:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_BT_470_NTSC;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_BT470BG:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_BT_601_PAL;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_SMPTE170M:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_BT_601_NTSC;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_SMPTE240M:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_SMPTE_240M;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_FILM:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_GENERIC_FILM;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_BT2020:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_BT_2020;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_SMPTEST428:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_SMPTE_ST_428;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_SMPTERP431:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_SMPTE_RP_431;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_SMPTEEG432:
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_SMPTE_EG_432;
+      break;
+    case GST_VIDEO_COLOR_PRIMARIES_ADOBERGB:
+    case GST_VIDEO_COLOR_PRIMARIES_UNKNOWN:
+    default:
+      GST_WARNING_OBJECT (self,
+          "Provided color primaries %d (%s) are not supported", cinfo.primaries,
+          gst_video_colorimetry_to_string (&cinfo));
+      param.eColorPrimaries = OMX_ALG_VIDEO_COLOR_PRIMARIES_UNSPECIFIED;
+  }
+
+  GST_DEBUG_OBJECT (self, "Set color primaries to %d (%s)",
+      param.eColorPrimaries, gst_video_colorimetry_to_string (&cinfo));
+
+  err =
+      gst_omx_component_set_parameter (self->enc,
+      (OMX_INDEXTYPE) OMX_ALG_IndexParamVideoColorPrimaries, &param);
+  CHECK_ERR ("color-primaries");
+
+  return TRUE;
+}
 
 static gboolean
 set_zynqultrascaleplus_props (GstOMXVideoEnc * self)
@@ -2751,6 +2813,8 @@ gst_omx_video_enc_set_format (GstVideoEncoder * encoder,
 
 #ifdef USE_OMX_TARGET_ZYNQ_USCALE_PLUS
   gst_omx_video_enc_set_latency (self);
+  if (!gst_omx_video_enc_set_color_primaries (self))
+    return FALSE;
 #endif
 
   self->downstream_flow_ret = GST_FLOW_OK;
