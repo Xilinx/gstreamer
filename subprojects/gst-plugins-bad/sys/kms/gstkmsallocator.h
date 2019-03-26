a9cb5898aba913b9a0435fd82ed7ae7605a441ed
Hyun Kwon <hyun.kwon@xilinx.com>
2019-03-25 18:12:50 -0700

    kmssink: Add the gst_kms_memory_remove_old_fb()
    Removing the fb that is currently used disables the entire display
    pipeline, so keep one fb as old one.
    This is rather simple way to fix by keeping the old fb, otherwise
    additional new APIs would be needed to remove the old fb after
    the new fb is swapped in.
    
    Upstream-status: Inappropriate
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>

--- subprojects/gst-plugins-bad/sys/kms/gstkmsallocator.h
+++ subprojects/gst-plugins-bad/sys/kms/gstkmsallocator.h
@@ -56,6 +56,7 @@ struct _GstKMSMemory
   GstMemory parent;
 
   guint32 fb_id;
+  guint32 old_fb_id;
   guint32 gem_handle[GST_VIDEO_MAX_PLANES];
   gsize mem_offsets[GST_VIDEO_MAX_PLANES];
   struct kms_bo *bo;
