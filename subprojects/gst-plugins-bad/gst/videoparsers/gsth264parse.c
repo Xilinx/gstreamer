Xilinx/gst-plugins-bad

h264parse: set DELTA UNIT on codec buffer
The AUD nal should have the DELTA UNIT if the associated buffer has it.
The decoder used to consider all the AUD nal as keyframe which was
obviously wrong.

Upstream-Status: Pending
Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>

gst/videoparsers/gsth264parse.c
@@ -2268,6 +2268,10 @@ gst_h264_parse_push_codec_buffer (GstH264Parse * h264parse,
     GST_BUFFER_FLAG_UNSET (buffer, GST_BUFFER_FLAG_DISCONT);
   }
 
+  if (GST_BUFFER_FLAG_IS_SET (buffer, GST_BUFFER_FLAG_DELTA_UNIT)) {
+    GST_BUFFER_FLAG_SET (nal, GST_BUFFER_FLAG_DELTA_UNIT);
+  }
+
   GST_BUFFER_PTS (nal) = GST_BUFFER_PTS (buffer);
   GST_BUFFER_DTS (nal) = GST_BUFFER_DTS (buffer);
   GST_BUFFER_DURATION (nal) = 0;

Xilinx/gstreamer

subprojects/gst-plugins-bad/gst/videoparsers/gsth264parse.c
@@ -2738,6 +2738,10 @@ gst_h264_parse_push_codec_buffer (GstH264Parse * h264parse,
       map.data, map.size);
   gst_buffer_unmap (nal, &map);
 
+  if (GST_BUFFER_FLAG_IS_SET (buffer, GST_BUFFER_FLAG_DELTA_UNIT)) {
+    GST_BUFFER_FLAG_SET (wrapped_nal, GST_BUFFER_FLAG_DELTA_UNIT);
+  }
+
   GST_BUFFER_PTS (wrapped_nal) = GST_BUFFER_PTS (buffer);
   GST_BUFFER_DTS (wrapped_nal) = GST_BUFFER_DTS (buffer);
   GST_BUFFER_DURATION (wrapped_nal) = 0;