Xilinx/gst-omx

omxvideodec: add interlace/xlnll caps feature couple
In order to support xlnx low latency caps features and
interlace, need to add the couple too.

Upstream-Status: Invalid [Xilinx Specific]
Signed-off-by: Stéphane Cerveau <scerveau@collabora.com>

omx/gstomxvideodec.c
@@ -284,6 +284,9 @@ gst_omx_video_dec_class_init (GstOMXVideoDecClass * klass)
       "RGBA") "; "
 #endif
 #ifdef USE_OMX_TARGET_ZYNQ_USCALE_PLUS
+      GST_VIDEO_CAPS_MAKE_WITH_FEATURES (GST_CAPS_FEATURE_MEMORY_XLNX_LL ","
+      GST_CAPS_FEATURE_FORMAT_INTERLACED, GST_OMX_VIDEO_SUPPORTED_FORMATS)
+      ", interlace-mode = (string) alternate ; "
       GST_VIDEO_CAPS_MAKE_WITH_FEATURES (GST_CAPS_FEATURE_FORMAT_INTERLACED,
       GST_OMX_VIDEO_SUPPORTED_FORMATS)
       ", interlace-mode = (string) alternate ; "

Xilinx/gstreamer

subprojects/gst-omx/omx/gstomxvideodec.c
@@ -284,6 +284,9 @@ gst_omx_video_dec_class_init (GstOMXVideoDecClass * klass)
       "RGBA") "; "
 #endif
 #ifdef USE_OMX_TARGET_ZYNQ_USCALE_PLUS
+      GST_VIDEO_CAPS_MAKE_WITH_FEATURES (GST_CAPS_FEATURE_MEMORY_XLNX_LL ","
+      GST_CAPS_FEATURE_FORMAT_INTERLACED, GST_OMX_VIDEO_DEC_SUPPORTED_FORMATS)
+      ", interlace-mode = (string) alternate ; "
       GST_VIDEO_CAPS_MAKE_WITH_FEATURES (GST_CAPS_FEATURE_FORMAT_INTERLACED,
       GST_OMX_VIDEO_DEC_SUPPORTED_FORMATS)
       ", interlace-mode = (string) alternate ; "