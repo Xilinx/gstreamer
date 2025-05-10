c9c995d321fc126ef9ed90ea6aa93c1104fda121
Klymenko, Anatoliy <Anatoliy.Klymenko@amd.com>
2025-05-10 06:10:43 +0530

    kmssink: Ease up display mode matching
    Kmssink tries to match display mode to video frame dimensions exactly
    while mode setting. The whole caps negotiation may fail if upstream
    element adjusts frame height for some reason. For example AVC decoder
    requires the frame to be 16x16 (macro block size) aligned and thus would
    adjust 1920x1080 frame to 1920x1088. This change takes in acount such case
    and allows up to 16 lines slack on mode selection.
    
    The alternative fix for this issue would be to do not rely on video frame
    dimensions during buffers allocation and deduct buffer sizes from video
    format bpp, planes strides and offsets. Such a massive change would affect
    all memory allocators as well as other elements.
    
    Signed-off-by: Anatoliy Klymenko <anatoliy.klymenko@amd.com>
    Acked-by: Varunkumar Allagadapa <varunkumar.allagadapa@amd.com>

--- subprojects/gst-plugins-bad/sys/kms/gstkmssink.c
+++ subprojects/gst-plugins-bad/sys/kms/gstkmssink.c
@@ -889,8 +889,17 @@ configure_mode_setting (GstKMSSink * self, GstVideoInfo * vinfo)
   }
 
   for (i = 0; i < conn->count_modes; i++) {
-    if (conn->modes[i].vdisplay == GST_VIDEO_INFO_FIELD_HEIGHT (vinfo) &&
-        conn->modes[i].hdisplay == GST_VIDEO_INFO_WIDTH (vinfo)) {
+    gint video_width = GST_VIDEO_INFO_WIDTH (vinfo);
+    gint video_height = GST_VIDEO_INFO_FIELD_HEIGHT (vinfo);
+    if ((conn->modes[i].vdisplay == video_height ||
+         /*
+          * AVC and HEVC decoders require frame dimensions to be 16x16 aligned.
+          * Horizontal alignment is being handled with width + pitch, vertical
+          * padding will just extend frame height. Consider this while matching
+          * display modes against video dimensions.
+          */
+         GST_ROUND_UP_16(conn->modes[i].vdisplay) == video_height) &&
+        conn->modes[i].hdisplay == video_width) {
       vrefresh = conn->modes[i].clock * 1000.00 /
           (conn->modes[i].htotal * conn->modes[i].vtotal);
       if (GST_VIDEO_INFO_INTERLACE_MODE (vinfo) ==
